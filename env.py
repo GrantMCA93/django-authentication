af6234b73f72c850f502e0dbaa82107ab1924a06
Ubuntu <ubuntu@ip-172-31-41-106.eu-west-1.compute.internal>
2019-07-25 02:14:56 +0000

    working ecommerce site

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_Pk5ONCJOfc6RMhGwhHGA1DeA00Nx2FihbI")
+os.environ.setdefault("STRIPE_SECRET", "sk_test_vVYdI8zPFgTNQizj1Vl4AODx00j3btodN2")
+os.environ.setdefault("DATABASE_URL", "postgres://clvacukgtqkkva:ae6b38e86694be9f6011ee0127a052e44b8268f9e741e790915851e9ff0558d8@ec2-54-228-246-214.eu-west-1.compute.amazonaws.com:5432/d7m7jbgj9kifk")
+os.environ.setdefault("SECRET_KEY", "@6_#m$-vus0j7sb^4s!adi1=cc1_^#y-=b&iz!dz7fp)c*+0xy")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAZRL5WVE72G4FPPOK")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
